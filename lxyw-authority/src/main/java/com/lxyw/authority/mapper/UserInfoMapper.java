2bd85c96ac74f04922975064c9c341735ec39f56
KenCheng <kencheng@foxmail.com>
2019-01-25 16:04:09 +0800

    增加注入配置

--- lxyw-authority/src/main/java/com/lxyw/authority/mapper/UserInfoMapper.java
+++ lxyw-authority/src/main/java/com/lxyw/authority/mapper/UserInfoMapper.java
@@ -2,7 +2,9 @@ package com.lxyw.authority.mapper;
 
 import com.lxyw.authority.entity.UserInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper
+@Component
 public interface UserInfoMapper extends CommonMapper<UserInfo>{
 }
